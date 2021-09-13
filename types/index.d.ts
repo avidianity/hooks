5055b0b82f02af5633c16426263b116789894ebf
John Michael Manlupig <manlupigjohnmichael@gmail.com>
2021-09-13 17:13:37 +0800

    Add new hooks

--- types/index.d.ts
+++ types/index.d.ts
@@ -7,3 +7,49 @@ export function useMode(): ['Add' | 'Edit', React.Dispatch<React.SetStateAction<
 export function useNullable<T>(data?: T): [T | null, React.Dispatch<React.SetStateAction<T | null>>];
 
 export function useArray<T>(data?: T[]): [T[], React.Dispatch<React.SetStateAction<T[]>>];
+
+export type UseArrayComplexReturn<T> = {
+	array: T[];
+	set: React.Dispatch<React.SetStateAction<T[]>>;
+	push: (element: T) => void;
+	filter: (predicate: (value: T, index: number, array: T[]) => value is T, thisArg?: T[]) => void;
+	update: (index: number, element: T) => void;
+	remove: (index: number) => void;
+	clear: () => void;
+};
+
+export function useArrayComplex<T>(data?: T[]): UseArrayComplexReturn<T>;
+
+export function usePartial<T extends object>(data?: Partial<T>): [Partial<T>, React.Dispatch<React.SetStateAction<Partial<T>>>];
+
+export function useToggle(defaultValue: boolean): [boolean, React.Dispatch<React.SetStateAction<boolean | undefined>>];
+
+export type UseTimeoutReturn = { set: Function; clear: Function };
+
+export function useTimeout(callback: () => void, delay: number): UseTimeoutReturn;
+
+export function useDebounce(callback: () => void, delay: number, dependencies?: React.DependencyList): void;
+
+export function useUpdateEffect(callback: () => void, dependencies?: React.DependencyList): void;
+
+export function usePrevious<T>(value: T): T;
+
+export type UseStorageReturn<T> = {
+	value: T;
+	setValue: React.Dispatch<React.SetStateAction<T>>;
+	remove: () => void;
+};
+
+export function useLocalStorage<T>(key: string, defaultValue: T): UseStorageReturn<T>;
+
+export function useSessionStorage<T>(key: string, defaultValue: T): UseStorageReturn<T>;
+
+export function useStorage<T>(key: string, defaultValue: T, storage: Storage): UseStorageReturn<T>;
+
+export type UseAsyncReturn<T> = {
+	value?: T;
+	loading: boolean;
+	error?: any;
+};
+
+export function useAsync<T>(callback: () => Promise<T>, dependencies?: React.DependencyList): UseAsyncReturn<T>;
