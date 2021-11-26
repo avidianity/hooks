bedf777e87002f70cdc8563176e4d7b23b5a3b38
John Michael Manlupig <manlupigjohnmichael@gmail.com>
2021-11-26 11:56:40 +0800

    Version 1.0.0

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,7 +1,5 @@
 import React from 'react';
 
-export function useURL(): (path: string) => string;
-
 export function useMode(): ['Add' | 'Edit', React.Dispatch<React.SetStateAction<'Add' | 'Edit'>>];
 
 export function useNullable<T>(data?: T): [T | null, React.Dispatch<React.SetStateAction<T | null>>];
